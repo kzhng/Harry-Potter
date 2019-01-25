106c52c45e9b8c94c57684b9f362a55b4a8242d1
mmoh0006 <mmoh0006@student.monash.edu>
2019-01-25 12:14:34 +1100

    avadakedavra spell and its effect

---
+++ FIT2099_Assignment_2018_S2/src/harrypotter/spells/AvadaKedavra.java
@@ -0,0 +1,40 @@
+package harrypotter.spells;
+
+import harrypotter.HPEntityInterface;
+import harrypotter.Spell;
+import harrypotter.HPActor;
+
+/**
+ * Class that represents Avada Kedavra, a type of <code>Spell</code> that can be casted in the HP world.
+ * When casted upon a target actor, <code>AvadaKedavra</code> kills it instantly.
+ * 
+ * @author Shakeel Rafi
+ */
+public class AvadaKedavra extends Spell {
+
+	/**
+	 * Constructor for <code>AvadaKedavra</code> class. Will initialize the name, description and generic result (afterEffect) of <code>AvadaKedavra</code>.
+	 * 
+	 */
+	public AvadaKedavra() {
+		super("Avada Kedavra", "Kill target instantly", "has been killed!");
+	}
+	
+	/**
+	 * Method that carries out the effect of <code>AvadaKedavra</code>, which casts a HPEntityInterface to a HPActor and deal damage equivalent to the HPActor's hit points.
+	 * 
+	 * @param theTarget the target <code>HPEntityInterface</code> for the Spell to be casted on
+	 */
+	@Override
+	public void spellEffect(HPEntityInterface theTarget) {
+		
+		HPActor targetActor = null;
+		
+		if (theTarget instanceof HPActor){
+			targetActor = (HPActor)theTarget;
+			
+			int targetHitPoints = targetActor.getHitpoints();
+			targetActor.takeDamage(targetHitPoints);
+		}
+	}
+}
